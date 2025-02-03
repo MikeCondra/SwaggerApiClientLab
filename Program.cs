a925aa8eafad44e1c7c1bc649ac96d749e335d3d
Michael <michael.condra@gmail.com>
2025-02-03 10:20:43 -0800

    First commit of files here

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        });
+
+
+        app.MapGet("/", () => $"Hello World!");
+
+        app.MapControllers();
+
+        await app.RunAsync();
+    }
+}
